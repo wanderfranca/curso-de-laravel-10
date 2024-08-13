cf184d843a4d3416c947c10a0e2a959e205d76df
wanderfranca <wander.wpf@hotmail.com>
2024-08-13 00:14:18 -0300

    atuaizacao: index com array

---
+++ app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
+use App\Models\Support;
+use App\Services\SupportService;
+use Illuminate\Http\Request;
+
+class SupportController extends Controller
+{
+    public function __construct(
+        protected SupportService $service
+    ) {}
+
+    public function index(Request $request)
+    {
+        $supports = $this->service->getAll($request->filter);
+        // dd($supports);
+
+        return view('admin.supports.index', compact('supports'));
+    }
+
+    public function create()
+    {
+        return view('admin.supports.create');    
+    }
+
+    public function store(StoreUpdateSupport $request, Support $support)
+    {
+        $this->service->new(
+            CreateSupportDTO::makeFromRequest($request) 
+        );
+
+        return redirect()->route('supports.index');
+    }
+
+    public function show(string $id)
+    {
+        if(!$support = $this->service->findOne($id)){
+            return back();
+        }
+        
+        return view('admin.supports.show', compact('support'));
+    }
+
+    public function edit(string $id)
+    {
+        if (!$support = $this->service->findOne($id)){
+            return back();
+        }
+
+        return view('admin.supports.edit', compact('support'));
+    }
+
+    public function update(Support $support, StoreUpdateSupport $request, string|int $id)
+    {
+        $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request) 
+        );
+
+        if(!$support){
+            return back();
+        }
+
+        return redirect()->route('supports.index');
+    }
+    
+    public function destroy(string $id)
+    {
+        $this->service->delete($id);
+        
+        return redirect()->route('supports.index');
+    }
+}
